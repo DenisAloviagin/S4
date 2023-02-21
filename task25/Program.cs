cf4aa7fe3c5c168e295f6f2e20ce16ab63d8ae57
denis aloviagin <d.aloviagin@gmail.com>
2023-02-22 03:41:04 +0500

    s4

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,34 +1,40 @@
-﻿int Prompt(string message)
+﻿internal class Program
 {
-    System.Console.Write(message);
-    string readInput = System.Console.ReadLine();
-    int result = int.Parse(readInput);
-    return result;
-}
-
-int Power(int powerBase, int exponent)
-{
-    int power = 1;
-    for (int i = 0; i < exponent; i++)
+    private static void Main(string[] args)
     {
-        power *= powerBase;
-    }
-    return power;
-}
+        int Prompt(string message)
+        {
+            Console.Write(message);
+            string readInput = System.Console.ReadLine();
+            int result = int.Parse(readInput);
+            return result;
+        }
 
-bool ValidateExponent(int exponent)
-{
-    if (exponent < 0)
-    {
-        System.Console.WriteLine("Показатель не должен быть меньше нуля");
-        return false;
-    }
-    return true;
-}
+        int Power(int powerBase, int exponent)
+        {
+            int power = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                power *= powerBase;
+            }
+            return power;
+        }
 
-int powerBase = Prompt("Введите основание: ");
-int exponent = Prompt("Введите показатель: ");
-if (ValidateExponent (exponent))
-{
-    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+        bool ValidateExponent(int exponent)
+        {
+            if (exponent < 0)
+            {
+                Console.WriteLine("Показатель не должен быть меньше нуля");
+                return false;
+            }
+            return true;
+        }
+
+        int powerBase = Prompt("Введите основание: ");
+        int exponent = Prompt("Введите показатель: ");
+        if (ValidateExponent(exponent))
+        {
+            Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+        }
+    }
 }
